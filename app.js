11a08a4ef73f70d9f13d0f4d2cae3ce31f3f4d0d
Dev <amir.moravej@gmail.com>
2014-05-30 13:09:06 -0400

    adding email system

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ stackTraceLimit = Infinity;
 var cluster = require('cluster'),
 	express = require("express"),
 	request = require("request"),
+	redis = require("redis"),
+	redisClient = redis.createClient(6379, '54.185.233.146'),
+	models = require("./models").config(redisClient),
 	app = express(),
     workers = {},
     cpuCount = require('os').cpus().length;
@@ -10,6 +13,8 @@ var forked = false;
 
 app.use(require('morgan')('dev'));
 app.set('port', process.env.PORT || 8000);
+app.use(require('body-parser')());
+app.use(require('method-override')());
 
 app.get('/call', function(req, res){
 	
@@ -23,9 +28,43 @@ app.get('/call', function(req, res){
 	  if (!error && response.statusCode == 200) {
 	    
 	    res.send(body);
+	    body = null;
 	  }
 	});
 });
+
+app.post('/email', function(req, res){
+
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    console.log(req.param('email'));
+
+    if (!isEmailAddress(req.param('email'))) return res.send(400);
+	
+	models.Email.findOne(req.body.email, function(error, exist) {
+		if (error) {
+			return res.send(500);
+		}
+
+		if (exist.length > 0) {
+			res.send(200);
+		}
+		else {
+			res.send(201);	
+		}
+	});	
+});
+
+// models.Email.findOne('test@example.com', function(error, exist) {
+// 	if (error) {
+// 		console.log(error);
+// 	}
+// 	console.log(exist);
+// 	if (exist.length > 0)
+// 		console.log(exist);
+// });
 	
 
 if (cluster.isMaster) {
@@ -51,3 +90,10 @@ function spawn(){
 	return worker;
 };
 
+
+function isEmailAddress(email) {
+
+	var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+    
+    return pattern.test(email);    
+};
---
+++ models.js
@@ -0,0 +1,66 @@
+var self = this,
+	mongoose = require('mongoose'),
+	crypto = require('crypto'),
+	redisClient,
+	connectionString = "mongodb://admin:IuT603JamshEqplE2N&0}x!@candidate.19.mongolayer.com:10061/dbk";
+
+
+function _objectId() {
+	var id = (new mongoose.Types.ObjectId).toString();
+	id = crypto.createHash('sha1').update(id).digest('hex');
+	return (new Buffer(id).toString('base64').replace(/=/g, ""));
+};
+
+function config (r) {
+	redisClient = r;
+	return self;
+};
+
+function cipher (text) {
+	// change the key
+	var key = 'NmU5MTgzYzJhNjM1N2JkZjhhMjAxZDc5OWM0ODFlZDYzMTYxNmQ3Ng';
+
+	var cipher = crypto.createCipher('aes-256-cbc', key);
+	var crypted = cipher.update(text, 'utf8', 'hex');
+	crypted += cipher.final('hex');
+	return crypted;		
+};
+
+function decipher(text){
+	var key = 'NmU5MTgzYzJhNjM1N2JkZjhhMjAxZDc5OWM0ODFlZDYzMTYxNmQ3Ng';
+	var decipher = crypto.createDecipher('aes-256-cbc', key);
+	var dec = decipher.update(text,'hex','utf8');
+	dec += decipher.final('utf8');
+	return dec;
+};
+
+function findOneEmail(email, callback) {
+	
+	// email = cipher(email);
+
+	redisClient.keys('email:'+email, function(error, exist){
+		if (error) {
+			if (callback) callback(error);
+			console.log("ERROR: error in finding email");
+			console.log(error);
+		}
+
+		if(callback) callback(null, exist);
+	});
+};
+
+function ready(callback) {
+	mongoose.connect(connectionString, function(err){
+		if (err) throw err;
+		callback();
+	});
+};
+
+var Email = {
+	findOne: findOneEmail
+};
+
+exports.config = config;
+exports.id = _objectId;
+exports.ready = ready;
+exports.Email = Email;
